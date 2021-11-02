7e4bf405320757b4fee522ebbe1a3ab81d709a71
AlexGin <Meinepostkam@gmail.com>
2021-11-02 21:58:34 +0300

    Add func

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewRed: UIView!
+    @IBOutlet weak var viewYellow: UIView!
+    @IBOutlet weak var viewGreen: UIView!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    @IBOutlet var viewCollection: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        for viewItem in viewCollection {
+            viewItem.layer.cornerRadius = 75
+            viewItem.alpha = 0.3
+        }
+        
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func runTrafficLights() {
+        if viewRed.alpha < 100 && viewYellow.alpha < 100 && viewGreen.alpha < 100 {
+            viewRed.alpha = 100
+        }else if viewRed.alpha == 100 {
+            viewRed.alpha = 0.3
+            viewYellow.alpha = 100
+        }else if viewYellow.alpha == 100 {
+            viewYellow.alpha = 0.3
+            viewGreen.alpha = 100
+        }else {
+            viewGreen.alpha = 0.3
+            viewRed.alpha = 100
+        }
+    }
+    
 }
 
